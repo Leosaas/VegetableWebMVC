ebaa1b649f49f545d52040f32dce071f956928df
Leosaas <anhldt2001@gmail.com>
2023-05-07 15:16:32 +0700

    7/5/20233 3:16

---
+++ VegetableWebMVC/Models/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Collections.Generic;
+using Infrastructure.Entities;
+
+namespace VegetableWebMVC.Models
+{
+	public class ProductViewModel
+	{
+
+	
+		[DisplayName("ID")]
+		public int pID { get; set; }
+		[DisplayName("Tên rau củ")]
+		
+		[MaxLength(125, ErrorMessage = "Tên không quá 125 ký tự.")]
+        [Required(ErrorMessage = "Tên rau củ không được bỏ trống")]
+        public string pName { get; set; }
+		[DisplayName("Giá bán")]
+		[Range(1000,999999,ErrorMessage = "Giá bán phải lớn hơn 1000")]
+		public double pPrice { get; set; }
+		[DisplayName("Số lượng tồn kho")]
+		public int pStorageQuantity { get; set; }
+		[DisplayName("Mã đơn vị tính")]
+		public int cID { get; set; }
+		[DisplayName("Đơn vị tính")]
+		public int unID { get; set; }
+		[DisplayName("Tên đơn vị tính")]
+		[MaxLength(125, ErrorMessage = "Tên không quá 125 ký tự.")]
+		public string unName { get; set; }
+		public IEnumerable<Unit> Units { get; set; }
+	}
+}
